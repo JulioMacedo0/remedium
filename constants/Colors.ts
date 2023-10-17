5aca5980b65ec3eb6be4b0be4e66964b8608917d
Julio Macedo <juliocesar1076@gmail.com>
2023-10-17 11:55:14 -0300

    feat: change color tokens

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,23 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#292929",
+    background: "#F1F5F9",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
+    tabBackground: "#ffffff",
+    borderColor: "#CBD5E1",
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#020617",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
+    tabBackground: "#1F2937",
+    borderColor: "#4B5563",
   },
 };
