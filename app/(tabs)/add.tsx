55ec4bc2cc31b2a2a1eb1e352e2dea1e0e2db1c8
Julio Macedo <juliocesar1076@gmail.com>
2023-10-17 11:55:46 -0300

    feat: change file name

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 import { Text, View } from "../../components/Themed";
 
-export default function TabTwoScreen() {
+export default function Add() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Remedy</Text>
