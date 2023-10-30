5e5c4bbbfa6d0ac1dea534e3b0f602cc418daa03
Julio Macedo <juliocesar1076@gmail.com>
2023-10-30 17:50:43 -0300

    feat: remove

--- src/context/theme-context/index.tsx
+++ src/context/theme-context/index.tsx
@@ -1,25 +1,6 @@
 import { createContext, useContext, useEffect, useRef, useState } from "react";
-import {
-  View,
-  useColorScheme,
-  Dimensions,
-  StyleSheet,
-  Image,
-  Button,
-} from "react-native";
+import { View, useColorScheme } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import { StatusBar } from "expo-status-bar";
-import { captureRef } from "react-native-view-shot";
-import Animated, {
-  Easing,
-  Extrapolate,
-  interpolate,
-  runOnJS,
-  useSharedValue,
-  withSequence,
-  withTiming,
-} from "react-native-reanimated";
 
 const wait = async (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
@@ -44,8 +25,6 @@ const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
 
 export function ThemeProvider(props: ProviderProps) {
   const imageRef = useRef<View>(null);
-  const [overlay1, setOverlay1] = useState<string>("");
-  const [overlay2, setOverlay2] = useState<string>("");
 
   const [selectedtheme, setSelectedTheme] =
     useState<SelectedTheme>("automatic");
@@ -68,9 +47,6 @@ export function ThemeProvider(props: ProviderProps) {
   }, []);
 
   const changeTheme = async ({ theme, x, y }: changeThemeArgs) => {
-    const snapshot1 = await captureRef(imageRef, { quality: 1 });
-
-    setOverlay1(snapshot1);
     setSelectedTheme(theme);
 
     try {
@@ -86,30 +62,6 @@ export function ThemeProvider(props: ProviderProps) {
         <ThemeContext.Provider value={{ theme, selectedtheme, changeTheme }}>
           {props.children}
         </ThemeContext.Provider>
-      </View>
-
-      <View
-        style={{
-          width: 300,
-          height: 300,
-        }}
-      >
-        <Button title="Test" />
-        {overlay1 && (
-          <Animated.Image
-            style={[
-              {
-                // position: "absolute",
-                // top: 0,
-                // left: 0,
-                // zIndex: 1,
-                width: "100%",
-                height: "100%",
-              },
-            ]}
-            source={{ uri: overlay1 }}
-          />
-        )}
       </View>
     </View>
   );
