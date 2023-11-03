177c331ff5a62db576e7dc22f209be98b7bb44ea
Julio Macedo <juliocesar1076@gmail.com>
2023-11-03 09:56:42 -0300

    feat: add modal

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,28 +1,19 @@
-import {
-  StyleSheet,
-  Modal,
-  Button,
-  TouchableWithoutFeedback,
-} from "react-native";
-import { View, Text } from "@/components";
+import { StyleSheet } from "react-native";
+import { View } from "@/components";
 import { useEffect, useState } from "react";
 import * as Notifications from "expo-notifications";
 
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Colors } from "@/constants";
 import { useTheme } from "@/context";
+import { PermissionModal } from "@/components/Modal/permissionModal";
 
 export default function Home() {
+  const { theme } = useTheme();
   const [modalVisible, setModalVisible] = useState(false);
 
   const closeModal = () => {
     setModalVisible(false);
   };
-  const openModal = () => {
-    setModalVisible(true);
-  };
 
-  const { theme } = useTheme();
   useEffect(() => {
     (async function () {
       const { status: existingStatus } =
@@ -43,51 +34,13 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => openModal()}>
-        <Text> open</Text>
-      </TouchableOpacity>
-      <Modal
+      <PermissionModal
         onRequestClose={() => closeModal()}
         visible={modalVisible}
         animationType="fade"
         transparent
         statusBarTranslucent
-      >
-        <TouchableWithoutFeedback onPress={closeModal}>
-          <View
-            style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "rgba(0,0,0,0.6)",
-            }}
-          >
-            <TouchableWithoutFeedback>
-              <View
-                style={{
-                  width: "85%",
-
-                  borderRadius: 8,
-                  paddingVertical: 24,
-                  paddingHorizontal: 12,
-                  backgroundColor: Colors[theme].tabBackground,
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Text>Suas notificações estão desativadas !</Text>
-                <Button
-                  title="close"
-                  onPress={() => {
-                    setModalVisible(false);
-                    console.log("press");
-                  }}
-                />
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
+      />
     </View>
   );
 }
@@ -95,14 +48,5 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
