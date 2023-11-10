4cd26d38045235a1fca847bd9fe723be75325474
Julio Macedo <juliocesar1076@gmail.com>
2023-11-10 16:18:01 -0300

    feat: make notify card

--- src/components/NotificationCard/index.tsx
+++ src/components/NotificationCard/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import { View } from "@components/View";
 import * as Notifications from "expo-notifications";
 import { Text } from "@components/Text";
@@ -47,42 +47,138 @@ export const NotificationCard = ({ Notification }: NotificationCardProps) => {
 
   return (
     <View
-      style={{
-        backgroundColor: Colors[theme].tabBackground,
-        padding: 15,
-        borderRadius: 15,
-        borderWidth: 1,
-        borderColor: Colors[getInvertedTheme()].tabBackground,
-      }}
+      style={[
+        styles.card,
+        {
+          borderColor: Colors[getInvertedTheme()].tabBackground,
+        },
+      ]}
     >
-      <Text>{Notification.content.title}</Text>
-      <Text>{Notification.content.subtitle}</Text>
-      <Text>{Notification.content.body}</Text>
-      <Text>{dataFormatada}</Text>
-      <Text>{Notification.trigger.type}</Text>
-      <Button
-        title="delete"
-        onPress={() => {
-          Notifications.cancelScheduledNotificationAsync(
-            Notification.identifier
-          );
-        }}
-      />
-      <Button
-        title="View"
-        onPress={async () => {
-          const id = await Notifications.scheduleNotificationAsync({
-            content: {
-              title: Notification.content.title,
-              subtitle: Notification.content.subtitle,
-              body: Notification.content.body,
-              data: Notification.content.data,
-            },
-            trigger: null,
-          });
-          console.log(id);
-        }}
-      />
+      <View
+        style={[
+          styles.header,
+          {
+            backgroundColor: Colors[theme].tabBarActiveTintColor,
+          },
+        ]}
+      >
+        <Text style={[styles.titleButton]}>{Notification.content.title}</Text>
+        <Text>{Notification.trigger.type}</Text>
+      </View>
+
+      <View
+        style={[
+          styles.bodyView,
+          {
+            backgroundColor: Colors[theme].tabBackground,
+          },
+        ]}
+      >
+        <Text>{Notification.content.body}</Text>
+
+        <View transparent style={[styles.row, styles.footer]}>
+          <View transparent style={[styles.row, styles.trigger]}>
+            <Text>{dataFormatada}</Text>
+          </View>
+
+          <View
+            transparent
+            style={[
+              styles.row,
+              {
+                alignSelf: "flex-end",
+                gap: 6,
+              },
+            ]}
+          >
+            <TouchableOpacity
+              style={[
+                styles.deleteButton,
+                {
+                  backgroundColor: "#f00",
+                },
+              ]}
+              onPress={() => {
+                Notifications.cancelScheduledNotificationAsync(
+                  Notification.identifier
+                );
+              }}
+            >
+              <Text style={styles.titleButton}>DELETE</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[
+                styles.viewButton,
+                {
+                  backgroundColor: Colors[theme].tabBarActiveTintColor,
+                },
+              ]}
+              onPress={async () => {
+                const id = await Notifications.scheduleNotificationAsync({
+                  content: {
+                    title: Notification.content.title,
+                    subtitle: Notification.content.subtitle,
+                    body: Notification.content.body,
+                    data: Notification.content.data,
+                  },
+                  trigger: null,
+                });
+                console.log(id);
+              }}
+            >
+              <Text style={styles.titleButton}>VIEW</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    overflow: "hidden",
+    borderRadius: 15,
+    borderWidth: 1,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  header: {
+    padding: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  footer: {
+    marginTop: 35,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  viewButton: {
+    paddingVertical: 7,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  deleteButton: {
+    paddingVertical: 7,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleButton: {
+    color: "#fff",
+  },
+  bodyView: {
+    paddingTop: 5,
+    paddingBottom: 15,
+    paddingHorizontal: 10,
+  },
+  trigger: {
+    gap: 5,
+  },
+});
