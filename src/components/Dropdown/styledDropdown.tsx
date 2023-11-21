430cb57ccbcf19dcf0f52432e385c75f2563048b
Julio Macedo <juliocesar1076@gmail.com>
2023-11-21 16:13:24 -0300

    feat: add border radius

--- src/components/Dropdown/styledDropdown.tsx
+++ src/components/Dropdown/styledDropdown.tsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import { Dropdown } from "react-native-element-dropdown";
 
 import { DropdownProps } from "react-native-element-dropdown/lib/typescript/components/Dropdown/model";
 
 export const StyledDropdown = <T,>(props: DropdownProps<T>) => {
+  console.log(" render StyledDropdown");
+  const [visible, setVisible] = useState(false);
   return (
     <Dropdown
       style={{
         backgroundColor: "#fff",
         padding: 8,
-        borderRadius: 8,
+        borderTopLeftRadius: visible ? 22 : 22,
+        borderTopRightRadius: visible ? 22 : 22,
+        borderBottomLeftRadius: visible ? 0 : 22,
+        borderBottomRightRadius: visible ? 0 : 22,
       }}
       containerStyle={{
-        borderRadius: 8,
         overflow: "hidden",
+        borderBottomRightRadius: 24,
+        borderBottomLeftRadius: 24,
       }}
       {...props}
+      onBlur={() => {
+        setVisible(false);
+      }}
+      onFocus={() => {
+        setVisible(true);
+      }}
     />
   );
 };
