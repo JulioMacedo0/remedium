72392c0670f98e90fd93644696dc44223ac0d444
Julio Macedo <juliocesar1076@gmail.com>
2023-11-22 16:46:52 -0300

    feat: get style from props

--- src/components/TextInput/styledTextInput.tsx
+++ src/components/TextInput/styledTextInput.tsx
@@ -4,19 +4,21 @@ import { TextInput } from "react-native";
 
 type StyledTextInputProps = TextInput["props"];
 
-export const StyledTextInput = ({ ...props }: StyledTextInputProps) => {
+export const StyledTextInput = ({ style, ...props }: StyledTextInputProps) => {
   const { theme, getInvertedTheme } = useTheme();
   return (
     <TextInput
+      style={[
+        {
+          textAlign: "center",
+          padding: 12,
+          backgroundColor: Colors[theme].tabBackground,
+          color: Colors[getInvertedTheme()].background,
+          borderRadius: 999,
+        },
+        style,
+      ]}
       {...props}
-      placeholder={Colors[getInvertedTheme()].background}
-      style={{
-        textAlign: "center",
-        padding: 12,
-        backgroundColor: Colors[theme].tabBackground,
-        color: Colors[getInvertedTheme()].background,
-        borderRadius: 999,
-      }}
     />
   );
 };
