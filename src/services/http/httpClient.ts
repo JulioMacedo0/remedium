de83e34f0ffe792d5be5e83c195a49b6194aeee8
Julio Macedo <juliocesar1076@gmail.com>
2024-01-24 08:15:19 -0300

    feat: add axios config

--- src/services/http/httpClient.ts
+++ src/services/http/httpClient.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:3000/";
+export const baseURL = "http://localhost:3000";
 
 const client = axios.create({
-  baseURL: `${baseURL}/api/v1`,
+  baseURL: `${baseURL}/api/v1/`,
 });
 
 export default client;
