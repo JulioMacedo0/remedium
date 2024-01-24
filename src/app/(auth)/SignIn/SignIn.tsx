dfcfdaf92870839f9aac516b25e236f5c6181bc9
Julio Macedo <juliocesar1076@gmail.com>
2024-01-24 16:35:03 -0300

    feat: create SignIn page

---
+++ src/app/(auth)/SignIn/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "@/components";
+
+export const SignIn = () => {
+  return (
+    <View
+      style={{
+        display: "flex",
+        flex: 1,
+      }}
+    >
+      <Text>Register page</Text>
+    </View>
+  );
+};
