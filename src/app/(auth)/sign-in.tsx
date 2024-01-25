31fc77b979e409e1e73d286421a375f368a42fe4
Julio Macedo <juliocesar1076@gmail.com>
2024-01-25 17:11:51 -0300

    feat: change color inputs

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -10,41 +10,69 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "@/components/Button";
 import { Link } from "expo-router";
-import { KeyboardAvoidingView, Platform } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { KeyboardAvoidingView } from "react-native";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
+import { useTheme } from "@/context";
+import { Colors } from "@/constants";
 
 const SignIn = () => {
-  return (
-    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
-      <KeyboardAvoidingView>
-        <ScrollView contentContainerStyle={{ alignItems: "center" }}>
-          <InputSection
-            title="Email"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <StyledTextInput placeholder="put your email...." />
-          </InputSection>
-          <InputSection
-            title="Password"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <StyledTextInput placeholder="put your password...." />
-          </InputSection>
+  const { theme } = useTheme();
 
-          <Button text="sign in" onPress={() => console.log("press")} />
-          <Link
-            style={{
-              width: "80%",
-              color: "blue",
-              textAlign: "right",
-            }}
-            replace
-            href="/(auth)/sign-up"
-          >
-            Dont have account?
-          </Link>
-        </ScrollView>
-      </KeyboardAvoidingView>
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: Colors[theme].tabBackground,
+      }}
+    >
+      <ThemedStatusBar />
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <KeyboardAvoidingView>
+          <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+            <InputSection title="Email">
+              <TextInput
+                style={{
+                  textAlign: "center",
+                  borderRadius: 999,
+                  padding: 12,
+                  backgroundColor: "#fff",
+                  color: "#000",
+                }}
+                placeholder="put your email...."
+              />
+            </InputSection>
+            <InputSection
+              title="Password"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <TextInput
+                style={{
+                  textAlign: "center",
+                  borderRadius: 999,
+                  padding: 12,
+                  backgroundColor: "#fff",
+                  color: "#000",
+                }}
+                placeholder="put your password...."
+              />
+            </InputSection>
+
+            <Button text="sign in" onPress={() => console.log("press")} />
+            <Link
+              style={{
+                width: "80%",
+                color: "blue",
+                textAlign: "right",
+              }}
+              replace
+              href="/(auth)/sign-up"
+            >
+              Dont have account?
+            </Link>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </View>
     </SafeAreaView>
   );
 };
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,56 +1,99 @@
 import React from "react";
-import { InputSection, StyledTextInput, Text, View } from "@/components";
+import {
+  InputSection,
+  StyledTextInput,
+  Text,
+  ThemedStatusBar,
+  View,
+} from "@/components";
 import { Link } from "expo-router";
-import { KeyboardAvoidingView, Platform } from "react-native";
+import { KeyboardAvoidingView, Platform, TextInput } from "react-native";
 import { Button } from "@/components/Button";
 import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "@/constants";
+import { useTheme } from "@/context";
 
 const SignOut = () => {
+  const { theme } = useTheme();
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
-      <KeyboardAvoidingView>
-        <ScrollView
-          contentContainerStyle={{
-            alignItems: "center",
-          }}
-        >
-          <InputSection
-            title="Username"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <StyledTextInput placeholder="put your username...." />
-          </InputSection>
-          <InputSection
-            title="Email"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <StyledTextInput placeholder="put your email...." />
-          </InputSection>
-          <InputSection
-            title="Password"
-            style={{ backgroundColor: "transparent" }}
-          >
-            <StyledTextInput
-              secureTextEntry
-              placeholder="put your password...."
-            />
-          </InputSection>
-
-          <Button text="sign up" onPress={() => console.log("press")} />
-          <Link
-            style={{
-              width: "80%",
-              color: "blue",
-              textAlign: "right",
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: Colors[theme].tabBackground,
+      }}
+    >
+      <ThemedStatusBar />
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <KeyboardAvoidingView>
+          <ScrollView
+            contentContainerStyle={{
+              alignItems: "center",
             }}
-            replace
-            href="/(auth)/sign-in"
           >
-            have account?
-          </Link>
-        </ScrollView>
-      </KeyboardAvoidingView>
+            <InputSection
+              title="Username"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <TextInput
+                style={{
+                  textAlign: "center",
+                  borderRadius: 999,
+                  padding: 12,
+                  backgroundColor: "#fff",
+                  color: "#000",
+                }}
+                placeholder="put your username...."
+              />
+            </InputSection>
+            <InputSection
+              title="Email"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <TextInput
+                style={{
+                  textAlign: "center",
+                  borderRadius: 999,
+                  padding: 12,
+                  backgroundColor: "#fff",
+                  color: "#000",
+                }}
+                placeholder="put your email...."
+              />
+            </InputSection>
+            <InputSection
+              title="Password"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <TextInput
+                style={{
+                  textAlign: "center",
+                  borderRadius: 999,
+                  padding: 12,
+                  backgroundColor: "#fff",
+                  color: "#000",
+                }}
+                secureTextEntry
+                placeholder="put your password...."
+              />
+            </InputSection>
+
+            <Button text="sign up" onPress={() => console.log("press")} />
+            <Link
+              style={{
+                width: "80%",
+                color: "blue",
+                textAlign: "right",
+              }}
+              replace
+              href="/(auth)/sign-in"
+            >
+              have account?
+            </Link>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </View>
     </SafeAreaView>
   );
 };
