2b5006f50868c9a0e37cc8a4cb6a50182ae011bb
Julio Macedo <juliocesar1076@gmail.com>
2024-02-08 16:18:59 -0300

    feat:  add validations

--- src/app/(tabs)/add.tsx
+++ src/app/(tabs)/add.tsx
@@ -25,34 +25,91 @@ import {
   SelectTrigger,
   ScrollView,
   Text,
+  Button,
+  ButtonText,
 } from "@gluestack-ui/themed";
 import { IntervalForm } from "@/forms/intervalForm";
+import { useForm, Controller, useWatch } from "react-hook-form";
+import { Alert, AlertSchema } from "@/schema/alert-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 export default function Add() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<Alert>({
+    resolver: zodResolver(AlertSchema),
+  });
+
+  const triggerType = useWatch({ control, name: "trigger.alertType" });
+
+  const onSubmit = (data: Alert) => {
+    console.log(data);
+  };
+
   return (
     <KeyboardAvoidingView px={10} py={10} style={{ flex: 1, zIndex: 999 }}>
       <ScrollView>
-        <Select>
-          <SelectTrigger variant="rounded" size="md" px={10} py={8}>
-            <SelectInput placeholder="Hows frequency?" />
-            <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
-          </SelectTrigger>
-          <SelectPortal>
-            <SelectBackdrop />
-            <SelectContent>
-              <SelectDragIndicatorWrapper>
-                <SelectDragIndicator />
-              </SelectDragIndicatorWrapper>
-              <SelectItem label="Interval" value="INTERVAL">
-                <MaterialIcons name="add-chart" size={24} color="black" />
-              </SelectItem>
-              <SelectItem label="Weekly" value="WEEKLY" />
-              <SelectItem label="Daily" value="DAILY" />
-              <SelectItem label="Date" value="DATE" />
-            </SelectContent>
-          </SelectPortal>
-        </Select>
+        <Text>{}</Text>
+        <Controller
+          control={control}
+          name="trigger.alertType"
+          render={({ field: { onChange, onBlur, value = "" } }) => (
+            <Select
+              isRequired
+              isInvalid={!!errors.trigger?.alertType?.message}
+              onValueChange={onChange}
+              onClose={onBlur}
+              selectedValue={value}
+            >
+              <SelectTrigger variant="rounded" size="md" px={10} py={8}>
+                <SelectInput placeholder="Hows frequency?" />
+                <MaterialIcons
+                  name="keyboard-arrow-down"
+                  size={24}
+                  color="black"
+                />
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectDragIndicatorWrapper>
+                    <SelectDragIndicator />
+                  </SelectDragIndicatorWrapper>
+                  <SelectItem label="Interval" value="INTERVAL">
+                    <MaterialIcons name="add-chart" size={24} color="black" />
+                  </SelectItem>
+                  <SelectItem label="Weekly" value="WEEKLY" />
+                  <SelectItem label="Daily" value="DAILY" />
+                  <SelectItem label="Date" value="DATE" />
+                </SelectContent>
+              </SelectPortal>
 
-        <IntervalForm />
+              <Text
+                mb={8}
+                color="$red600"
+                fontWeight="$medium"
+                fontSize={"$sm"}
+                textAlign="center"
+              >
+                {errors.trigger?.alertType?.message}
+              </Text>
+            </Select>
+          )}
+        />
+
+        <IntervalForm control={control} errors={errors} />
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          isDisabled={false}
+          isFocusVisible={false}
+          onPress={handleSubmit(onSubmit)}
+        >
+          <ButtonText>Add </ButtonText>
+        </Button>
       </ScrollView>
     </KeyboardAvoidingView>
   );
