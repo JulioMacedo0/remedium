84c1a9dd40a35f883f2219d9dfc63b7ca30d5557
Julio Macedo <juliocesar1076@gmail.com>
2024-02-08 18:01:13 -0300

    feat: add validations

--- src/forms/commomForm/commomForm.tsx
+++ src/forms/commomForm/commomForm.tsx
@@ -44,11 +44,6 @@ export const CommomForm = ({ control, errors }: ICommomFormProps) => {
                 onBlur={onBlur}
               />
             </Input>
-            {/* <FormControlHelper>
-    <FormControlHelperText>
-      Must be at least 6 characters.
-    </FormControlHelperText>
-  </FormControlHelper> */}
             <FormControlError>
               {/* <FormControlErrorIcon as={AlertCircleIcon} /> */}
               <FormControlErrorText>
@@ -59,57 +54,81 @@ export const CommomForm = ({ control, errors }: ICommomFormProps) => {
         )}
       />
 
-      <FormControl
-        size="md"
-        isDisabled={false}
-        isInvalid={false}
-        isReadOnly={false}
-        isRequired={true}
-      >
-        <FormControlLabel>
-          <FormControlLabelText>Subtitle</FormControlLabelText>
-        </FormControlLabel>
-        <Input>
-          <InputField type="text" placeholder="Subtitle" />
-        </Input>
-        {/* <FormControlHelper>
+      <Controller
+        control={control}
+        name="subtitle"
+        render={({ field: { onChange, onBlur, value = "" } }) => (
+          <FormControl
+            size="md"
+            isDisabled={false}
+            isInvalid={!!errors.subtitle}
+            isReadOnly={false}
+            isRequired={true}
+          >
+            <FormControlLabel>
+              <FormControlLabelText>Subtitle</FormControlLabelText>
+            </FormControlLabel>
+            <Input>
+              <InputField
+                type="text"
+                placeholder="Subtitle"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            </Input>
+            {/* <FormControlHelper>
           <FormControlHelperText>
             Must be at least 6 characters.
           </FormControlHelperText>
         </FormControlHelper> */}
-        <FormControlError>
-          {/* <FormControlErrorIcon as={AlertCircleIcon} /> */}
-          <FormControlErrorText>
-            At least 6 characters are required.
-          </FormControlErrorText>
-        </FormControlError>
-      </FormControl>
+            <FormControlError>
+              {/* <FormControlErrorIcon as={AlertCircleIcon} /> */}
+              <FormControlErrorText>
+                {errors.subtitle?.message}
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+        )}
+      />
 
-      <FormControl
-        size="md"
-        isDisabled={false}
-        isInvalid={false}
-        isReadOnly={false}
-        isRequired={true}
-      >
-        <FormControlLabel>
-          <FormControlLabelText>Body</FormControlLabelText>
-        </FormControlLabel>
-        <Input>
-          <InputField type="text" placeholder="Body" />
-        </Input>
-        {/* <FormControlHelper>
+      <Controller
+        control={control}
+        name="body"
+        render={({ field: { onChange, onBlur, value = "" } }) => (
+          <FormControl
+            size="md"
+            isDisabled={false}
+            isInvalid={!!errors.body}
+            isReadOnly={false}
+            isRequired={true}
+          >
+            <FormControlLabel>
+              <FormControlLabelText>Body</FormControlLabelText>
+            </FormControlLabel>
+            <Input>
+              <InputField
+                type="text"
+                placeholder="Body"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            </Input>
+            {/* <FormControlHelper>
           <FormControlHelperText>
             Must be at least 6 characters.
           </FormControlHelperText>
         </FormControlHelper> */}
-        <FormControlError>
-          {/* <FormControlErrorIcon as={AlertCircleIcon} /> */}
-          <FormControlErrorText>
-            At least 6 characters are required.
-          </FormControlErrorText>
-        </FormControlError>
-      </FormControl>
+            <FormControlError>
+              {/* <FormControlErrorIcon as={AlertCircleIcon} /> */}
+              <FormControlErrorText>
+                {errors.body?.message}
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+        )}
+      />
     </Fragment>
   );
 };
