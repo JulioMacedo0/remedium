9d0e9035647d62f6e957d26ff264b296a89cee5d
Julio Macedo <juliocesar1076@gmail.com>
2024-02-14 19:33:47 -0300

    feat: add request interceptor to add token in Authorization

--- src/services/http/httpClient.ts
+++ src/services/http/httpClient.ts
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { storageService } from "../storage/storageService";
+import { STORAGE_KEYS } from "../storage/storegesKeys";
 
 const baseURL = "http://192.168.1.8:3000";
 
@@ -6,3 +8,15 @@ export const client = axios.create({
   baseURL: `${baseURL}/api/v1/`,
   timeout: 30000,
 });
+
+client.interceptors.request.use(
+  async function (config) {
+    const token = await storageService.getItem<string>(STORAGE_KEYS.TOKEN);
+    config.headers.Authorization = `Baerer ${token}`;
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
