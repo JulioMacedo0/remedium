b863829ed1c7b7711dfec899bc5aeca869b8d39f
Julio Macedo <juliocesar1076@gmail.com>
2024-02-22 09:50:21 -0300

    feat: remove unused code

--- src/components/AlertCard/index.tsx
+++ src/components/AlertCard/index.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@/context";
 import { CreateAlertResponse } from "@/stores/alert/userAlertStore";
 import { Text, View, Heading, HStack, Icon } from "@gluestack-ui/themed";
+import { router } from "expo-router";
 import { Pill, FilePenLine } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 type AlertCardProps = {
@@ -10,15 +11,13 @@ type AlertCardProps = {
 export type weeksValues = 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
 export const AlertCard = ({ alert }: AlertCardProps) => {
-  const { theme, getInvertedTheme } = useTheme();
-  console.log(alert);
   return (
     <View bgColor="#b6a3f5" p={12} rounded="$lg">
       <HStack alignItems="center" justifyContent="space-between">
         <Icon as={Pill} color="#fff" size="lg" />
 
         <Heading color="#fff">{alert.title}</Heading>
-        <TouchableOpacity onPress={() => console.log("FilePenLine")}>
+        <TouchableOpacity onPress={() => router.push("/edit-alert")}>
           <Icon as={FilePenLine} color="#fff" size="lg" />
         </TouchableOpacity>
       </HStack>
