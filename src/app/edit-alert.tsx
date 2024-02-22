86bf5d531649145a56e08e9a994ce9a3f83eb558
Julio Macedo <juliocesar1076@gmail.com>
2024-02-22 11:58:30 -0300

    feat: create edit-alert

---
+++ src/app/edit-alert.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@gluestack-ui/themed";
+
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function EditAlert() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Teste</Text>
+    </SafeAreaView>
+  );
+}
