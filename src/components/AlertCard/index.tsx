426467e9fbe51be8b81fc423a3863d6270143617
Julio Macedo <juliocesar1076@gmail.com>
2024-02-26 18:01:29 -0300

    feat: pass id to params in route

--- src/components/AlertCard/index.tsx
+++ src/components/AlertCard/index.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@/context";
 import { CreateAlertResponse } from "@/stores/alert/userAlertStore";
 import { Text, View, Heading, HStack, Icon } from "@gluestack-ui/themed";
-import { router } from "expo-router";
+import { useRouter } from "expo-router";
 import { Pill, FilePenLine } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 type AlertCardProps = {
@@ -11,12 +11,17 @@ type AlertCardProps = {
 export type weeksValues = 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
 export const AlertCard = ({ alert }: AlertCardProps) => {
+  const router = useRouter();
   return (
     <View bgColor="#b6a3f5" p={12} rounded="$lg">
       <HStack alignItems="center" justifyContent="space-between">
         <Icon as={Pill} color="#fff" size="lg" />
         <Heading color="#fff">{alert.title}</Heading>
-        <TouchableOpacity onPress={() => router.push("/edit-alert")}>
+        <TouchableOpacity
+          onPress={() => {
+            router.push(`/edit-alert?id=${alert.id}`);
+          }}
+        >
           <Icon as={FilePenLine} color="#fff" size="lg" />
         </TouchableOpacity>
       </HStack>
