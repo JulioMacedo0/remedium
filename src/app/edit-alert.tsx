bce232fde4bde8a3f648cc5b39bd65f3c9e86267
Julio Macedo <juliocesar1076@gmail.com>
2024-02-26 18:01:18 -0300

    feat: return correct form depende id params

--- src/app/edit-alert.tsx
+++ src/app/edit-alert.tsx
@@ -2,28 +2,28 @@ import { DailyForm } from "@/forms/daily-form/daily-form";
 import { DateForm } from "@/forms/date-form/date-form";
 import { IntervalForm } from "@/forms/interval-form/interval-form";
 import { WeeklyForm } from "@/forms/weekly-from/weekly-form";
-import { CreateAlertType } from "@/schema";
 import { useAlertStore } from "@/stores/alert/userAlertStore";
-import { Text } from "@gluestack-ui/themed";
+import { KeyboardAvoidingView, ScrollView, Text } from "@gluestack-ui/themed";
 import { useLocalSearchParams } from "expo-router";
 
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 export default function EditAlert() {
   const { alerts } = useAlertStore((set) => set);
   const { id } = useLocalSearchParams<{ id: string }>();
 
   const renderForms = () => {
     const selectedAlert = alerts.find((alert) => alert.id == id);
+
+    console.log("id poggers", id);
+    console.log("console porra", selectedAlert);
     switch (selectedAlert?.trigger.alertType) {
-      // case "INTERVAL":
-      //   return <IntervalForm setAlertType={ChangeAlertType} />;
-      // case "WEEKLY":
-      //   return <WeeklyForm setAlertType={ChangeAlertType} />;
+      case "INTERVAL":
+        return <IntervalForm initialValue={selectedAlert} />;
+      case "WEEKLY":
+        return <WeeklyForm initialValue={selectedAlert} />;
       case "DAILY":
         return <DailyForm initialValue={selectedAlert} />;
-      // case "DATE":
-      //   return <DateForm  />;
+      case "DATE":
+        return <DateForm initialValue={selectedAlert} />;
       default:
         return (
           <Text>Ops, ocorreu um erro, seu alerta não foi encontrado.</Text>
@@ -31,5 +31,9 @@ export default function EditAlert() {
     }
   };
 
-  return <Text>Teste</Text>;
+  return (
+    <KeyboardAvoidingView px={10} py={10} style={{ flex: 1, zIndex: 999 }}>
+      <ScrollView>{renderForms()}</ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
