4526c66aee6c5e58db5d15cc394f2bf6f31c3db5
Julio Macedo <juliocesar1076@gmail.com>
2024-03-03 18:18:44 -0300

    feat: add new seection to handle permission

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet } from "react-native";
+import { Linking, StyleSheet, AppState, AppStateStatus } from "react-native";
 import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+import { BellRing, BellOffIcon } from "lucide-react-native";
 import { useTheme, useI18n } from "@/context";
 import BottomSheet, { BottomSheetBackdrop } from "@gorhom/bottom-sheet";
-import { useCallback, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { BottomSheetDefaultBackdropProps } from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types";
+import * as Notifications from "expo-notifications";
 import {
   View,
   Text,
@@ -20,6 +22,8 @@ import { ThemedStatusBar } from "@/components/ThemedStatusBar";
 import { useAuthStore } from "@/stores/auth/useAuthStore";
 
 export default function Config() {
+  const [permission, setPermission] = useState("");
+
   const { selectedtheme, changeTheme, theme } = useTheme();
   const { i18n, changeLaguange } = useI18n();
 
@@ -29,6 +33,13 @@ export default function Config() {
 
   const openBottomSheet = () => bottomSheetRef.current?.expand();
 
+  const getNotificationPermission = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+    setPermission(status);
+  };
+  const openSettings = async () => {
+    await Linking.openSettings();
+  };
   const renderBackdrop = useCallback(
     (props: BottomSheetDefaultBackdropProps) => (
       <BottomSheetBackdrop
@@ -78,6 +89,25 @@ export default function Config() {
     });
 
   const { logout } = useAuthStore((set) => set);
+
+  useEffect(() => {
+    getNotificationPermission();
+
+    const handleAppStateChange = (nextAppState: AppStateStatus) => {
+      if (nextAppState === "active") {
+        getNotificationPermission();
+      }
+    };
+
+    const subscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -132,6 +162,19 @@ export default function Config() {
           icon={<Ionicons name="exit-outline" size={23} color={"red"} />}
           text={"Exit"}
           onPress={() => logout()}
+        />
+      </Section>
+      <Section title={"Notification"}>
+        <SectionItem
+          icon={
+            permission != "granted" ? (
+              <BellOffIcon size={23} color={Colors[theme].text} />
+            ) : (
+              <BellRing size={23} color={Colors[theme].text} />
+            )
+          }
+          text={permission != "granted" ? "Disabled" : "Active"}
+          onPress={() => openSettings()}
         />
       </Section>
       <BottomSheet
