8ab610e22a71a0fc9e0440d0e1f8ac23b4235395
Julio Macedo <juliocesar1076@gmail.com>
2024-04-18 14:29:23 -0300

    feat: add persist in theme

--- src/stores/theme/use-theme-store.ts
+++ src/stores/theme/use-theme-store.ts
@@ -1,5 +1,7 @@
+import { storageService } from "@/services/storage/storageService";
+import { STORAGE_KEYS } from "@/services/storage/storegesKeys";
 import { create } from "zustand";
-
+import { persist, createJSONStorage } from "zustand/middleware";
 type ThemeType = "light" | "dark";
 
 interface useThemeStoreType {
@@ -7,9 +9,17 @@ interface useThemeStoreType {
   changeTheme: (theme: ThemeType) => void;
 }
 
-export const useThemeStore = create<useThemeStoreType>((set) => ({
-  theme: "light",
-  changeTheme: (themeValue) => {
-    set(() => ({ theme: themeValue }));
-  },
-}));
+export const useThemeStore = create<useThemeStoreType>(
+  persist(
+    (set) => ({
+      theme: "light",
+      changeTheme: (themeValue) => {
+        set(() => ({ theme: themeValue }));
+      },
+    }),
+    {
+      name: STORAGE_KEYS.THEME,
+      storage: createJSONStorage(() => storageService),
+    }
+  )
+);
