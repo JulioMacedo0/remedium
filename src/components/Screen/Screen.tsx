b80c815859884c7dfa0d3d361952c360da86e943
Julio Macedo <juliocesar1076@gmail.com>
2024-04-18 14:29:43 -0300

    feat: add status bar in Screen

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,12 +1,16 @@
 import { Box } from "@/constants/theme/theme";
+import { useThemeStore } from "@/stores/theme/use-theme-store";
+import { StatusBar } from "expo-status-bar";
 
 type ScreenProps = {
   children: React.ReactNode;
 };
 
 export const Screen = ({ children }: ScreenProps) => {
+  const themeValue = useThemeStore((set) => set.theme);
   return (
     <Box backgroundColor="mainBackground" flex={1} pt="xl" px="s">
+      <StatusBar style={themeValue === "dark" ? "light" : "dark"} />
       {children}
     </Box>
   );
