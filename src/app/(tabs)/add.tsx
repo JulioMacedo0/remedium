5b89b7253d652885ef196629013e28a1b5f78f6f
Julio Macedo <juliocesar1076@gmail.com>
2024-04-18 17:19:19 -0300

    feat: use Select component

--- src/app/(tabs)/add.tsx
+++ src/app/(tabs)/add.tsx
@@ -1,25 +1,10 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import {
-  KeyboardAvoidingView,
-  Select,
-  SelectBackdrop,
-  SelectContent,
-  SelectDragIndicator,
-  SelectDragIndicatorWrapper,
-  SelectInput,
-  SelectItem,
-  SelectPortal,
-  SelectTrigger,
-  ScrollView,
-  Text,
-  SelectItemText,
-} from "@gluestack-ui/themed";
+import { KeyboardAvoidingView, ScrollView, Text } from "@gluestack-ui/themed";
 import { IntervalForm } from "@/forms/interval-form/interval-form";
 import { WeeklyForm } from "@/forms/weekly-from/weekly-form";
 import { DailyForm } from "@/forms/daily-form/daily-form";
 import { DateForm } from "@/forms/date-form/date-form";
 import { useAlertTypeStore } from "@/stores/use-alert-type-store/use-alert-type-store";
-import { Screen } from "@/components";
+import { Screen, Select } from "@/components";
 import { Theme } from "@/constants";
 import { useTheme } from "@shopify/restyle";
 
@@ -52,34 +37,7 @@ export default function Add() {
     <Screen>
       <KeyboardAvoidingView style={{ flex: 1, zIndex: 999 }}>
         <ScrollView>
-          {!alertType && (
-            <Select onValueChange={changeAlertType}>
-              <SelectTrigger variant="rounded" size="md">
-                <SelectInput
-                  placeholder="Hows frequency?"
-                  placeholderTextColor={text}
-                  color={text}
-                />
-                <MaterialIcons
-                  name="keyboard-arrow-down"
-                  size={24}
-                  color={text}
-                />
-              </SelectTrigger>
-              <SelectPortal>
-                <SelectBackdrop />
-                <SelectContent bg={tabBackground}>
-                  <SelectDragIndicatorWrapper>
-                    <SelectDragIndicator bg={text} />
-                  </SelectDragIndicatorWrapper>
-                  <SelectItem label="Interval" value="INTERVAL" />
-                  <SelectItem label="Weekly" value="WEEKLY" />
-                  <SelectItem label="Daily" value="DAILY" />
-                  <SelectItem label="Date" value="DATE" />
-                </SelectContent>
-              </SelectPortal>
-            </Select>
-          )}
+          {!alertType && <Select onValueChange={changeAlertType} />}
 
           {renderForms()}
         </ScrollView>
