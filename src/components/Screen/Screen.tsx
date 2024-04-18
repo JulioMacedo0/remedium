af2619f49a98aa322b24fd4978e2273e422fc9e9
Julio Macedo <juliocesar1076@gmail.com>
2024-04-18 11:10:16 -0300

    feat: add padding

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -6,7 +6,7 @@ type ScreenProps = {
 
 export const Screen = ({ children }: ScreenProps) => {
   return (
-    <Box backgroundColor="mainBackground" flex={1}>
+    <Box backgroundColor="mainBackground" flex={1} pt="xl" px="s">
       {children}
     </Box>
   );
