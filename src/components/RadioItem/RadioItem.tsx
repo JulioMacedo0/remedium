3ba265a1a2a43ab813cd845964e3599695b970ad
Julio Macedo <juliocesar1076@gmail.com>
2024-04-18 11:09:44 -0300

    feat: resolve imports

---
+++ src/components/RadioItem/RadioItem.tsx
@@ -0,0 +1,64 @@
+import { Pressable } from "react-native";
+
+import { ReactNode } from "react";
+import { Box, Colors, Text } from "@/constants";
+
+type RadioItemProps = {
+  icon: ReactNode;
+  text: string;
+  isSelected: boolean;
+  onPress?: () => void;
+  withoutFeedback?: boolean;
+};
+
+export const RadioItem = ({
+  icon,
+  text,
+  isSelected,
+  onPress,
+  withoutFeedback,
+}: RadioItemProps) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [
+        {
+          opacity: withoutFeedback ? 1 : pressed ? 0.6 : 1,
+          paddingHorizontal: 15,
+          paddingVertical: 15,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        },
+      ]}
+    >
+      <Box flexDirection="row" alignItems="center">
+        {icon}
+        <Text ml="l" color="text">
+          {text}
+        </Text>
+      </Box>
+      <Box
+        bg="tabBackground"
+        borderColor="text"
+        style={{
+          borderWidth: 1,
+          width: 23,
+          height: 23,
+          borderRadius: 999,
+          padding: 4,
+        }}
+      >
+        {isSelected ? (
+          <Box
+            bg="text"
+            flex={1}
+            style={{
+              borderRadius: 999,
+            }}
+          />
+        ) : null}
+      </Box>
+    </Pressable>
+  );
+};
---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import { Box, Text } from "@/constants/theme";
+
+type SectionProps = {
+  title: string;
+  children: ReactNode;
+};
+
+export const Section = ({ title, children }: SectionProps) => {
+  return (
+    <Box>
+      <Text color="text" variant="body">
+        {title}
+      </Text>
+      <Box
+        mt="l"
+        mb="l"
+        bg="tabBackground"
+        borderRadius={8}
+        borderWidth={1}
+        borderColor="tabBackground"
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
