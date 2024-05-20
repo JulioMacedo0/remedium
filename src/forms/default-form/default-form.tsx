63254171f4507bae31459ae34e603453b520f30b
Julio Macedo <juliocesar1076@gmail.com>
2024-05-20 10:21:12 -0300

    feat: creat default form to avoid DRY

---
+++ src/forms/default-form/default-form.tsx
@@ -0,0 +1,108 @@
+import { Control, Controller, FieldErrors } from "react-hook-form";
+import { useI18nStore } from "@/stores/i18n/useI18nStore";
+import { InputForm } from "@/components";
+import { useRef } from "react";
+import { TextInput } from "react-native";
+
+type DefaultFormValues = {
+  title: string;
+  body: string;
+  subtitle: string;
+};
+
+type DefaultfomrProps = {
+  control: Control<DefaultFormValues>;
+  errors: FieldErrors<DefaultFormValues>;
+  initialValue: DefaultFormValues;
+  remedyNameInputRef: React.MutableRefObject<TextInput | null>;
+  onSubmitEditing: () => void;
+};
+
+export const DefaultForm = ({
+  control,
+  errors,
+  initialValue,
+  remedyNameInputRef,
+  onSubmitEditing,
+}: DefaultfomrProps) => {
+  const i18n = useI18nStore((state) => state.i18n);
+
+  const DoseNameInputRef = useRef<TextInput | null>(null);
+  const insctructionsRef = useRef<TextInput | null>(null);
+
+  return (
+    <>
+      <Controller
+        control={control}
+        name="title"
+        render={({ field: { onChange, onBlur, value = "" } }) => (
+          <InputForm
+            ref={remedyNameInputRef}
+            Label={i18n.t("FROMS.DEFAULT.NAME")}
+            ErrorText={errors.title?.message}
+            InputProps={{
+              onBlur: () => onBlur(),
+              onChangeText: (text) => onChange(text),
+              value,
+              placeholder: initialValue?.title ?? "Dipirona",
+              blurOnSubmit: false,
+              returnKeyType: "next",
+              onSubmitEditing: () => DoseNameInputRef.current?.focus(),
+              type: "text",
+            }}
+            FormControlProps={{
+              isInvalid: !!errors.title,
+            }}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="subtitle"
+        render={({ field: { onChange, onBlur, value = "" } }) => (
+          <InputForm
+            ref={DoseNameInputRef}
+            Label={i18n.t("FROMS.DEFAULT.DOSE")}
+            ErrorText={errors.subtitle?.message}
+            FormControlProps={{ isInvalid: !!errors.subtitle }}
+            InputProps={{
+              blurOnSubmit: false,
+              returnKeyType: "next",
+              onSubmitEditing: () => insctructionsRef.current?.focus(),
+              type: "text",
+              placeholder: initialValue?.subtitle ?? "1 pill",
+              onChangeText: (text) => onChange(text),
+              onBlur: () => onBlur,
+              value: value,
+            }}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="body"
+        render={({ field: { onChange, onBlur, value = "" } }) => (
+          <InputForm
+            ref={insctructionsRef}
+            Label={i18n.t("FROMS.DEFAULT.INSTRUCTIONS")}
+            ErrorText={errors.body?.message}
+            FormControlProps={{
+              isInvalid: !!errors.body,
+            }}
+            InputProps={{
+              returnKeyType: "done",
+              onSubmitEditing: () => onSubmitEditing(),
+              type: "text",
+              placeholder: initialValue?.body ?? "Take before breakfast",
+              onChangeText: (text) => onChange(text),
+              onBlur: () => onBlur,
+              value: value,
+            }}
+          />
+        )}
+      />
+    </>
+  );
+};
