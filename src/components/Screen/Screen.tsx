39dbeac15aa380062c519617deb3a3979789d5f9
Julio Macedo <juliocesar1076@gmail.com>
2024-05-20 17:02:34 -0300

    feat: remove margin x

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -9,7 +9,7 @@ type ScreenProps = {
 export const Screen = ({ children }: ScreenProps) => {
   const themeValue = useThemeStore((set) => set.theme);
   return (
-    <Box backgroundColor="mainBackground" flex={1} pt="xl" px="m">
+    <Box backgroundColor="mainBackground" flex={1} px="m">
       <StatusBar style={themeValue === "dark" ? "light" : "dark"} />
       {children}
     </Box>
