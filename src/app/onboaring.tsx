39cc0e854bbbfaacbea502ebcc796482b2430b5c
Julio Macedo <juliocesar1076@gmail.com>
2024-05-28 17:08:58 -0300

    feat: rename function

--- src/app/onboaring.tsx
+++ src/app/onboaring.tsx
@@ -2,24 +2,15 @@ import { OnboaringItem } from "@/components";
 import { OnboaringButton } from "@/components/OnboaringButton/OnboaringButton";
 import { onBoaringData } from "@/constants";
 import React, { useState } from "react";
-import {
-  View,
-  Button,
-  StyleSheet,
-  Text,
-  useWindowDimensions,
-} from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useSharedValue, withTiming } from "react-native-reanimated";
 
-import { SafeAreaView } from "react-native-safe-area-context";
 export default function Onboaring() {
-  const { height: SCREEN_HEIGHT } = useWindowDimensions();
-
   const buttonVal = useSharedValue(0);
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handlPress = () => {
+  const handlePress = () => {
     if (currentIndex == onBoaringData.length - 1) {
       return;
     }
@@ -27,7 +18,7 @@ export default function Onboaring() {
     setCurrentIndex((pervIndex) => pervIndex + 1);
     buttonVal.value = withTiming(buttonVal.value + 1);
   };
-  console.log(buttonVal.value);
+
   return (
     <View style={[style.container]}>
       <View>
@@ -37,7 +28,7 @@ export default function Onboaring() {
           );
         })}
       </View>
-      <OnboaringButton onPress={handlPress} buttonVal={buttonVal} />
+      <OnboaringButton onPress={() => handlePress()} buttonVal={buttonVal} />
     </View>
   );
 }
