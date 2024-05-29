bd016bf27c0b09c69f74c2fd243518b144cb2898
Julio Macedo <juliocesar1076@gmail.com>
2024-05-29 18:04:17 -0300

    feat: create snapshot and show in screen

--- src/app/onboaring.tsx
+++ src/app/onboaring.tsx
@@ -5,35 +5,58 @@ import { onBoaringData } from "@/constants";
 import { storageService } from "@/services/storage/storageService";
 import { STORAGE_KEYS } from "@/services/storage/storegesKeys";
 import { useRouter } from "expo-router";
-import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef, useState } from "react";
+import { View, StyleSheet, PixelRatio, Text } from "react-native";
 import { useSharedValue, withTiming } from "react-native-reanimated";
-
+import {
+  Canvas,
+  SkImage,
+  makeImageFromView,
+  Image,
+} from "@shopify/react-native-skia";
 export default function Onboaring() {
+  const pd = PixelRatio.get();
   const buttonVal = useSharedValue(0);
   const router = useRouter();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const ref = useRef(null);
+  const wait = async (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+  const [overlay, setOverlay] = useState<SkImage | null>(null);
 
-  const handlePress = () => {
+  const handlePress = async () => {
     if (currentIndex == onBoaringData.length - 1) {
       storageService.setItem(STORAGE_KEYS.VIEWONBOARING, "true");
       router.replace("/sign-in");
       return;
     }
-
+    const snapshot = await makeImageFromView(ref);
+    setOverlay(snapshot);
+    wait(80);
     setCurrentIndex((pervIndex) => pervIndex + 1);
     buttonVal.value = withTiming(buttonVal.value + 1);
   };
 
   return (
     <View style={[style.container]}>
-      <View>
+      <View ref={ref} collapsable={false}>
         {onBoaringData.map((item, index) => {
           return (
             currentIndex == index && <OnboaringItem item={item} key={index} />
           );
         })}
       </View>
+      {overlay && (
+        <Canvas style={StyleSheet.absoluteFillObject} pointerEvents="none">
+          <Image
+            image={overlay}
+            x={0}
+            y={0}
+            width={overlay.width() / pd}
+            height={overlay.height() / pd}
+          />
+        </Canvas>
+      )}
       <Pagination data={onBoaringData} buttonVal={buttonVal} />
       <OnboaringButton onPress={() => handlePress()} buttonVal={buttonVal} />
     </View>
