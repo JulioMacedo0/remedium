b547fbf45ad922682454fe6e91eb7422d99245c4
Julio Macedo <juliocesar1076@gmail.com>
2025-03-15 11:29:50 -0300

    feat:  create router

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+// import {AppStack} from './AppStack';
+import {AuthStack} from './AuthStack';
+
+const authenticated = true;
+
+export function Router() {
+  return <NavigationContainer>{<AuthStack />}</NavigationContainer>;
+}
